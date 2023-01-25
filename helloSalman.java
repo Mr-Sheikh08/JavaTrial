8ab954f3c115d240a96f61f2d8aced1f2e9f6f4f
Mr-Sheikh08 <salmanworld08@gmail.com>
2023-01-25 18:15:39 +0530

    My first commit

---
+++ helloSalman.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello Salman");
+	}
+}
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
